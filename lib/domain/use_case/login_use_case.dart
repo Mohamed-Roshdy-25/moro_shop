45373a42e311ce89114934da96527e8436f57d71
MoRoshdy <hsyn87857@gmail.com>
2023-02-19 07:05:47 +0200

    17- creating base use case and login use case

---
+++ lib/domain/use_case/login_use_case.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:moro_shop/data/network/failure.dart';
+import 'package:moro_shop/data/network/requests.dart';
+import 'package:moro_shop/domain/models/models.dart';
+import 'package:moro_shop/domain/repository/repository.dart';
+import 'package:moro_shop/domain/use_case/base_use_case.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput,LoginModel>{
+  final Repository _repository;
+
+  LoginUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, LoginModel>> execute(LoginUseCaseInput input) async {
+    return await _repository.login(LoginRequest(input.email, input.password));
+  }
+  
+}
+
+class LoginUseCaseInput{
+  String email;
+  String password;
+
+  LoginUseCaseInput(this.email, this.password);
+}
