409aef4263eb3272cfdd5c4ee4e20849631cd64e
MoRoshdy <hsyn87857@gmail.com>
2023-02-22 21:41:32 +0200

    20- creating state renderer and assets manager and font manager

---
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:moro_shop/app/constants.dart';
+import 'package:moro_shop/presentation/common/state_renderer/state_renderer.dart';
+import 'package:moro_shop/presentation/resources/strings_manager.dart';
+
+abstract class FlowState{
+  StateRendererType getStateRendererType();
+  String getMessage();
+}
+
+class LoadingState extends FlowState {
+  StateRendererType stateRendererType;
+  String? message;
+
+  LoadingState(
+      {required this.stateRendererType, String message = AppStrings.loading});
+
+  @override
+  String getMessage() => message ?? AppStrings.loading;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+// error state (popup, full_screen)
+
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+
+  ErrorState(this.stateRendererType, this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+// success state (popup)
+
+class SuccessState extends FlowState {
+  String message;
+
+  SuccessState(this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.popupSuccessState;
+}
+
+// content state
+
+class ContentState extends FlowState {
+  ContentState();
+
+  @override
+  String getMessage() => Constants.empty;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.contentState;
+}
+
+// empty state
+
+class EmptyState extends FlowState {
+  String message;
+
+  EmptyState(this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() =>
+      StateRendererType.fullScreenEmptyState;
+}
+
+extension FlowStateExtension on FlowState{
+   getScreenWidget(context,{Function? retryActionFunction}){
+    switch(runtimeType){
+      case LoadingState:
+        dismissDialog(context);
+        if(getStateRendererType() == StateRendererType.popupLoadingState) {
+          _showPopup(context, getStateRendererType(),);
+        }else{
+          return StateRenderer(stateRendererType: getStateRendererType());
+        }
+         break;
+      case ErrorState:
+        dismissDialog(context);
+        if(getStateRendererType() == StateRendererType.popupErrorState) {
+          _showPopup(context, getStateRendererType(),message: getMessage(),retryActionFunction: retryActionFunction);
+        }else{
+          return StateRenderer(stateRendererType: getStateRendererType());
+        }
+         break;
+      case SuccessState:
+        dismissDialog(context);
+        if(getStateRendererType() == StateRendererType.popupLoadingState) {
+          _showPopup(context, getStateRendererType(),message: getMessage());
+        }else{
+          return StateRenderer(stateRendererType: getStateRendererType());
+        }
+    }
+  }
+
+
+  _showPopup(
+      BuildContext context, StateRendererType stateRendererType,
+      {String? message,String title = Constants.empty, Function? retryActionFunction}) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => showDialog(
+        context: context,
+        builder: (context) => StateRenderer(
+          stateRendererType: stateRendererType,
+          message: message??AppStrings.loading,
+          title: title,
+          retryActionFunction: retryActionFunction ?? () {},
+        )));
+  }
+
+  _isCurrentDialogShowing(BuildContext context) =>
+      ModalRoute.of(context)?.isCurrent != true;
+
+  dismissDialog(BuildContext context) {
+    if (_isCurrentDialogShowing(context)) {
+      Navigator.of(context, rootNavigator: true).pop(true);
+    }
+  }
+}
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -4,7 +4,20 @@ class AppStrings {
   static const String requestCanceled = "request_canceled";
   static const String unKnownError = "unKnown_error";
   static const String cacheError = "cache_error";
-  static const String noInternetError = "no_internet_error";
+  static const String noInternetError = "no_internet_connection";
   static const String badCertificate = "bad_certificate";
   static const String connectionError = "connection_error";
+  static const String loading = "Loading...";
+  static const String ok = "ok";
+  static const String retryAgain = "Retry Again";
+  static const String success = "success";
+  static const String email = "E-mail";
+  static const String emailHintText = "Enter your email";
+  static const String emailErrorText = "E-mail is too short";
+  static const String password = "Password";
+  static const String passwordHintText = "Enter your password";
+  static const String passwordErrorText = "Password is too short";
+  static const String forgetPassword = "Forgot your password?";
+  static const String dontHaveAccount  = "Don't have an account? ";
+  static const String create  = 'Create';
 }
