a33a365d0de82ef2895c9bd6765e1d90ead54a46
MoRoshdy <hsyn87857@gmail.com>
2023-04-18 09:25:16 +0200

    27-refactor code and solve dependancy problem of dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:moro_shop/app/app.dart';
 import 'package:moro_shop/app/cache_helper.dart';
